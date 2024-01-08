f1ce2228593adba7729ce9a44e80ae6d5bc24320
priy13 <supri.aidc@gmail.com>
2024-01-08 13:51:07 +0700

    init

---
+++ js/script.js
@@ -0,0 +1,13 @@
+// Toggle class active
+const navbarNav = document.querySelector(".navbar-nav");
+document.querySelector("#hamburger-menu").onclick = () => {
+  navbarNav.classList.toggle("active");
+};
+
+// Klik diluar sidebar untuk menghilangkan navigasi
+const hamburger = document.querySelector("#hamburger-menu");
+document.addEventListener("click", function (e) {
+  if (!hamburger.contains(e.target) && !navbarNav.contains(e.target)) {
+    navbarNav.classList.remove("active");
+  }
+});
